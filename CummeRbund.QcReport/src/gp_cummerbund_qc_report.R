d8a4f2cd6bd4ab816b3acdd5055d463d44a07634
David Eby <eby@broadinstitute.org>
2013-12-02 03:35:00 +0000

    Fixed a bug where MDS plots don't work with fewer than 3 samples/replicates.  Updated to newer version of dependency installer.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/CummeRbund@32363 47fccdf2-e235-0410-87e5-63258e95d6e5

--- CummeRbund.QcReport/src/gp_cummerbund_qc_report.R
+++ CummeRbund.QcReport/src/gp_cummerbund_qc_report.R
@@ -15,34 +15,46 @@ GP.CummeRbund.QC.Report <- function(cuffdiff.job, gtf.file, genome.file, output.
    feature.selector <- get.feature.selector(feature.level)
 
    cuff <- readCufflinks.silent(cuffdiff.job, gtf.file, genome.file)
-   features <-feature.selector(cuff)
+   print(cuff)
+   features <- feature.selector(cuff)
+   
+   # Write out a table of the differentially expressed features
+   feature_diff_data <- diffData(features)
+   sig_feature_data <- subset(feature_diff_data, (significant == 'yes'))
+   write.table(sig_feature_data, paste0('sig_diffExp_', feature.level,'.txt'), sep='\t',
+               row.names = F, col.names = T, quote = F)
 
    ## Calls based on examples from http://compbio.mit.edu/cummeRbund/manual_2_0.html
-   disp<-dispersionPlot(features)
+   disp <- dispersionPlot(features)
    print.plotObject(disp, "Dispersion", device.open)
    
-   fpkm.scv<-fpkmSCVPlot(features)
+   fpkm.scv <- fpkmSCVPlot(features)
    print.plotObject(fpkm.scv, "FPKM.SCV", device.open)
 
-   dens<-csDensity(features, replicates=show.replicates, logMode=log.transform)
+   dens <- csDensity(features, replicates=show.replicates, logMode=log.transform)
    print.plotObject(dens, "Density", device.open) 
 
-   box<-csBoxplot(features, replicates=show.replicates, logMode=log.transform)
+   box <- csBoxplot(features, replicates=show.replicates, logMode=log.transform)
    print.plotObject(box, "Boxplot", device.open) 
 
-   # expose colorByStatus to user?
-   s<-csScatterMatrix(features, replicates=show.replicates, logMode=log.transform)
+   s <- csScatterMatrix(features, replicates=show.replicates, logMode=log.transform)
    print.plotObject(s, "Scatter", device.open) 
 
-   v<-csVolcanoMatrix(features)
+   v <- csVolcanoMatrix(features)
    print.plotObject(v, "Volcano", device.open) 
 
    # The significance matrix behaves differently in that it handles the selection within the call.
    sM <- sigMatrix(cuff, level=feature.level)
    print.plotObject(sM, "SignificanceMatrix", device.open) 
 
-   mds <- MDSplot(features, replicates=show.replicates, logMode=log.transform)
-   print.plotObject(mds, "DimensionalityReduction.mds", device.open) 
+   # The MDSplot throws an error when trying to plot fewer than two samples/replicates.
+   if ((!show.replicates && NROW(samples(cuff)) < 3) || (show.replicates && NROW(replicates(cuff)) < 3)) {
+      print("Skipping the MDS plot - too few samples/replicates")
+   }
+   else {
+      mds <- MDSplot(features, replicates=show.replicates, logMode=log.transform)
+      print.plotObject(mds, "DimensionalityReduction.mds", device.open)
+   }
 
    pca <- PCAplot(features, replicates=show.replicates)
    print.plotObject(pca, "DimensionalityReduction.pca", device.open) 
@@ -57,6 +69,6 @@ GP.CummeRbund.QC.Report <- function(cuffdiff.job, gtf.file, genome.file, output.
 
    # The dendrogram plots behave differently - apparently the print/plot call is embedded within.
    device.open("Dendrogram")
-   dend<-csDendro(features,replicates=show.replicates,logMode=log.transform)
+   dend <- csDendro(features,replicates=show.replicates,logMode=log.transform)
    dev.off()
 }
