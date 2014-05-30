9e3f3ee0b576ec83d4b497cac5d9e6073add37db
David Eby <eby@broadinstitute.org>
2014-05-30 21:27:45 +0000

    Removed some plots of dubious value.  Fixed a bug in handling conditions list.  Changed to require at least two conditions.  Removed geneset param as it was not used.  Overall simplification of code.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/CummeRbund@33651 47fccdf2-e235-0410-87e5-63258e95d6e5

--- CummeRbund.SelectedConditionsReport/src/gp_cummerbund_selected_conditions_report.R
+++ CummeRbund.SelectedConditionsReport/src/gp_cummerbund_selected_conditions_report.R
@@ -10,6 +10,12 @@
 
 GP.CummeRbund.SelectedConditions.Report <- function(cuffdiff.job, selected.conditions, gtf.file, genome, output.format, 
                                                     feature.level, report.as.aggregate, log.transform) {
+   # Verify we have enough conditions for plotting
+   conditions.count <- NROW(selected.conditions)
+   if (conditions.count < 2) {
+      stop("Too few conditions for plotting.  Must have two or more.") 
+   }
+
    use.replicates <- !report.as.aggregate
    device.open <- get.device.open(output.format)
 
@@ -18,50 +24,20 @@ GP.CummeRbund.SelectedConditions.Report <- function(cuffdiff.job, selected.condi
    cuff <- readCufflinks.silent(cuffdiff.job, gtf.file, genome)
    
    check.selected.conditions(selected.conditions, cuff)
-
-   # Get a CuffGeneSet based on the selected conditions and *all* genes (features).
-   #
-   # Possible future improvement: modify the underlying queries to work without the geneIdList.
-   #   That is quite complex but it would be much more efficient.  If the following works for
-   #   fairly large datasets, however, it's probably not worth the effort to make this change
-   #   as it will likely become a moot point when we re-implement the workflow with the new
-   #   Cufflinks 2.2.x improvements for working with subsets of conditions upstream.    
-   genesetForConditions <- getGenes(cuff, geneIdList=featureNames(cuff@genes), sampleIdList=selected.conditions)
-
-   selected.data <- genesetForConditions
-   if (feature.level != "genes") { selected.data <- feature.selector(genesetForConditions) }
-
-   # Write out the various plots
-   print.heatmap(selected.data, device.open, "SelectedConditions", use.replicates, log.transform)
-   print.expressonBarplot(selected.data, device.open, "SelectedConditions", use.replicates, log.transform)
-   print.expressonPlot(selected.data, device.open, "SelectedConditions", use.replicates, log.transform)
-   print.dendrogram(selected.data, device.open, "SelectedConditions", use.replicates, log.transform)
-   
+   selected.data <- feature.selector(cuff)
+      
    # Generate plots for pairwise sample comparisons
-   # This is skipped if no conditions were specified in selected.conditions due to the overhead and
-   # potential large number of files generated.  
-   if (conditions.count < 2) {
-      # Skip these if there is not at least one pair
-      print("Too few conditions; skipping pairwise plots") 
-   }
-   else {
-      for (i in 1:(conditions.count-1)) {
-         for (j in (i+1):conditions.count) {
-            currI <- selected.conditions[i]
-            currJ <- selected.conditions[j]
-            print.volcanoPlot(selected.data, currI, currJ, device.open, "SelectedConditions")
-            # Plot from next line is identical to the above even with args reversed.  This is an acknowledged bug in cummeRbund. 
-            #print.volcanoPlot(selected.data, currJ, currI, device.open, "SelectedConditions")
-            print.scatterPlot(selected.data, currI, currJ, device.open, "SelectedConditions", log.transform)
-            print.scatterPlot(selected.data, currJ, currI, device.open, "SelectedConditions", log.transform)
-         }
+   for (i in 1:(conditions.count-1)) {
+      for (j in (i+1):conditions.count) {
+         currI <- selected.conditions[i]
+         currJ <- selected.conditions[j]
+         print.volcanoPlot(selected.data, currI, currJ, device.open, "SelectedConditions")
+         # Plot from next line is identical to the above even with args reversed.  This is an acknowledged bug in cummeRbund. 
+         #print.volcanoPlot(selected.data, currJ, currI, device.open, "SelectedConditions")
+         print.scatterPlot(selected.data, currI, currJ, device.open, "SelectedConditions", log.transform)
+         print.scatterPlot(selected.data, currJ, currI, device.open, "SelectedConditions", log.transform)
+         print.MAplot(selected.data, currI, currJ, device.open, "SelectedConditions", log.transform)
+         print.MAplot(selected.data, currJ, currI, device.open, "SelectedConditions", log.transform)
       }
    }
 }
-
-# Move to Util
-print.heatmap <- build.standardPlotter("Heatmap", 
-   function(selected.data, use.replicates, log.transform) {
-      return(csHeatmap(selected.data, clustering='both', replicates=use.replicates, logMode=log.transform))
-   }
-)
